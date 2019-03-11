f3fe5accf51980cee0d0e26b38bbfa54e867cb32
Michael Andrews <mail@michaelandrews.net>
2019-03-10 17:18:56 -0700

    v1

--- eurorack_4_module_challenge.py
+++ eurorack_4_module_challenge.py
@@ -1,6 +1,27 @@
 #!/usr/bin/python
 
+# This script is inspired by YouTuber "Comparative Irrelevance" and their #3modulechallenge.
+# https://www.youtube.com/channel/UCQBQ6F7VD78Ic_FkTexeE8g
+# Use this to generate a list of four modules from a list in the same folder called 'modules.txt'.
+# The format of the modules.txt file is:
+#
+# <module_name>,<type>
+#   module_name: This is the name of the module.  Spaces are OK.
+#   type: This is the type of module.  Choose one of the following letters to categorize:
+#     f: FX (delays, reverbs)
+#     m: Modulator (function generators, LFOs, sequencers)
+#     s: Sound Source (oscillators, sample players)
+#     u: Utility (VCAs, EGs, quantizers, touch controllers)
+#
+# Anyways, I wanted to do four modules instead of three because mentally it just makes more sense
+# to me a the moment in how I think of modular.  Also, logically separating the modules into types 
+# ensure that you'll be able to make some noise at the very least.
+# 
+# I did add an optional switch to this script ("--ci") to allow you to do the true #3modulechallenge.  
+# It's kind of dumb though since it randomly chooses any three modules from the list provided and 
+# prints them out.  There's a chance you could end up with nothing but VCAs, for example.
 
+import argparse
 import os
 import random
 
@@ -8,9 +29,33 @@ import random
 MODULE_FILE = "./modules.txt"
 
 
-def generate_and_print_challenge(fx_list, modulators_list, sources_list):
-
+def generate_and_print_challenge(fx_list, modulators_list, sources_list, utilities_list):
+  random.shuffle(fx_list)
+  random.shuffle(modulators_list)
+  random.shuffle(sources_list)
+  random.shuffle(utilities_list)
+  print("Your four module challenge is:")
+  print("Sound Source: {}".format(sources_list.pop()))
+  print("Modulator:    {}".format(modulators_list.pop()))
+  print("FX:           {}".format(fx_list.pop()))
+  print("Utility:      {}".format(utilities_list.pop()))
+  print("\nGood luck!")
   return
+
+
+def generate_and_print_three_module_challenge():
+  module_list = []
+  with open(MODULE_FILE) as file:
+    for line in file.readlines():
+      split_line = line.strip('\n').split(',')
+      module_list.append(split_line[0])
+  file.close()
+  random.shuffle(module_list)
+  print("Your three module challenge is:")
+  for i in range(0, 3):
+    print("  {}".format(module_list.pop()))
+  return
+
 
 def generate_module_lists():
   fx_list = []
@@ -32,13 +77,18 @@ def generate_module_lists():
       else:
         utilities_list.append(split_line[0])
   file.close()
-  print(fx_list)
-  print(modulators_list)
-  print(sources_list)
+  #print(fx_list)
+  #print(modulators_list)
+  #print(sources_list)
+  #print(utilities_list)
   return fx_list, modulators_list, sources_list, utilities_list
 
 
 def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--ci", action="store_true")
+  args = parser.parse_args()
+  ci = args.ci
   # http://patorjk.com/software/taag/#p=display&h=1&v=1&f=Stop&t=Eurorack%0A4%20Module%0AChallenge
   print(""" _______                                         _       
 (_______)                                       | |      
@@ -59,9 +109,12 @@ def main():
 | \_____ | | | |( ( | || || |( (/ / | | | |( ( | |( (/ / 
  \______)|_| |_| \_||_||_||_| \____)|_| |_| \_|| | \____)
                                            (_____|       """)
-  fx_list, modulators_list, sources_list, utilities_list = generate_module_lists()
-  #print(module_list)
-  generate_and_print_challenge(fx_list, modulators_list, sources_list, utilities_list)
+  if ci:
+    generate_and_print_three_module_challenge()
+  else:
+    fx_list, modulators_list, sources_list, utilities_list = generate_module_lists()
+    #print(module_list)
+    generate_and_print_challenge(fx_list, modulators_list, sources_list, utilities_list)
   exit()
 
 
